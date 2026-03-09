0f3ed86ab462796e522ba425f94df8caf0714f26
ClerivanBF <clerivan.falcao@castilho.com.br>
2026-03-09 09:32:40 -0300

    Adicionados algumas variaveis que usam valores existentes. E criado função para criar o link da solicitação do Fluig do equipamento

--- Cadastro-de-Equipamentos-V2/workflow/scripts/wkfCadastroDeEquipamentos.servicetask19.js
+++ Cadastro-de-Equipamentos-V2/workflow/scripts/wkfCadastroDeEquipamentos.servicetask19.js
@@ -41,6 +41,8 @@ function cadastraEquipamentoNoSisma() {
 }
 
 function getDadosEquipamento() {
+    var urlSolicitacao = montaUrlSolicitacao();
+
     var CODCOLIGADA = hAPI.getCardValue("CODCOLIGADA");
     var CODCCUSTO = hAPI.getCardValue("CODCCUSTO");
     var PREFIXO = hAPI.getCardValue("prefixo");
@@ -62,6 +64,7 @@ function getDadosEquipamento() {
     var POTENCIAHP = hAPI.getCardValue("potenciaMotor");
 
     var DATACHEGADA = hAPI.getCardValue("dataChegadaObra").split("/").reverse().join("");
+    var MESANO = hAPI.getCardValue("dataChegadaObra").split("/").reverse().join("");
 
 
     var VALOR_MOBILIZADO = moneyToFloat(hAPI.getCardValue("valorMobilizacao"));
@@ -82,11 +85,15 @@ function getDadosEquipamento() {
     var INI_HODOMETRO = tipoKmChegadaObra == "KM" ? valorChegadaObra : "";
     var INI_HORIMETRO = tipoKmChegadaObra == "Horas" ? valorChegadaObra : "";
 
-    var tipoCombustivel = hAPI.getCardValue("tipoCombustivel");
+    var VIDABASE_KM = tipoKmChegadaObra == "KM" ? valorChegadaObra : "";
+    var VIDABASE_HORA = tipoKmChegadaObra == "Horas" ? valorChegadaObra : "";
 
+    var tipoCombustivel = hAPI.getCardValue("tipoCombustivel");
 
     var tipoConsumoMedio = hAPI.getCardValue("tipoConsumoMedio");
     var consumoMedio = hAPI.getCardValue("consumoMedio");
+
+    var CODIMEDI = tipoConsumoMedio == "km/L" ? 1 : 2;
 
     var CONSUMO_KM = tipoConsumoMedio == "km/L" ? consumoMedio : "";
     var CONSUMO_HORA = tipoConsumoMedio == "L/H" ? consumoMedio : "";
@@ -108,9 +115,13 @@ function getDadosEquipamento() {
     var QUANTIDADE = hAPI.getCardValue("quantidade");
 
     return {
+
+        "URL_SOLICITACAO" : urlSolicitacao + "",
+
         "CODCOLIGADA": CODCOLIGADA + "",
         "CODCCUSTO": CODCCUSTO + "",
         "PREFIXO": PREFIXO + "",
+        "CODIMEDI": CODIMEDI + "",
         "IDMODE": IDMODE + "",
         "CODICLME": CODICLME + "",
         "IDCLOP": IDCLOP + "",
@@ -125,6 +136,7 @@ function getDadosEquipamento() {
         "POTENCIAHP_UNID": POTENCIAHP_UNID + "",
         "POTENCIAHP": POTENCIAHP + "",
         "DATACHEGADA": DATACHEGADA + "",
+        "MESANO": MESANO + "",
 
         "VALOR_MOBILIZADO": VALOR_MOBILIZADO + "",
         "TIPO_VALOR_MOBILIZADO": TIPO_VALOR_MOBILIZADO + "",
@@ -137,26 +149,45 @@ function getDadosEquipamento() {
 
         "VALOR_MAODEOBRA": VALOR_MAODEOBRA + "",
 
-        INI_HODOMETRO: INI_HODOMETRO + "",
-        INI_HORIMETRO: INI_HORIMETRO + "",
-        CODIMATE_COMBUSTIVEL: tipoCombustivel + "",
+        "INI_HODOMETRO": INI_HODOMETRO + "",
+        "INI_HORIMETRO": INI_HORIMETRO + "",
 
-        CONSUMO_HORA: CONSUMO_HORA + "",
-        CONSUMO_KM: CONSUMO_KM + "",
+        "VIDABASE_KM": VIDABASE_KM + "",
+        "VIDABASE_HORA": VIDABASE_HORA + "",
 
-        CAPATANQ_ABAST: CAPATANQ_ABAST + "",
-        CODIUNID_CAPACIDADE_COMBUSTIVEL: CODIUNID_CAPACIDADE_COMBUSTIVEL + "",
+        "CODIMATE_COMBUSTIVEL": tipoCombustivel + "",
 
-        ANEXOS_DOCUMENTACAO: ANEXOS_DOCUMENTACAO + "",
-        ANEXOS_FOTOS: ANEXOS_FOTOS + "",
-        ANEXOS_LAUDO: ANEXOS_LAUDO + "",
-        ANEXOS_PLANO_MANUTENCAO: ANEXOS_PLANO_MANUTENCAO + "",
-        ANEXOS_ART: ANEXOS_ART + "",
+        "CONSUMO_HORA": CONSUMO_HORA + "",
+        "CONSUMO_KM": CONSUMO_KM + "",
 
-        DATA_VENCIMENTO_ART:DATA_VENCIMENTO_ART + "",
-        DATA_VENCIMENTO_LAUDO:DATA_VENCIMENTO_LAUDO + "",
+        "CAPATANQ_ABAST": CAPATANQ_ABAST + "",
+        "CODIUNID_CAPACIDADE_COMBUSTIVEL": CODIUNID_CAPACIDADE_COMBUSTIVEL + "",
 
-        QUANTIDADE:QUANTIDADE + "",
+        "ANEXOS_DOCUMENTACAO": ANEXOS_DOCUMENTACAO + "",
+        "ANEXOS_FOTOS": ANEXOS_FOTOS + "",
+        "ANEXOS_LAUDO": ANEXOS_LAUDO + "",
+        "ANEXOS_PLANO_MANUTENCAO": ANEXOS_PLANO_MANUTENCAO + "",
+        "ANEXOS_ART": ANEXOS_ART + "",
+
+        "DATA_VENCIMENTO_ART":DATA_VENCIMENTO_ART + "",
+        "DATA_VENCIMENTO_LAUDO":DATA_VENCIMENTO_LAUDO + "",
+
+        "QUANTIDADE":QUANTIDADE + "",
     };
 
+}
+
+function montaUrlSolicitacao() {
+    var urlSolicitacao = "";
+    var env = getServerURL();
+    var numSolicitacao = getValue("WKNumProces")
+
+    urlSolicitacao = env + "/portal/p/1/pageworkflowview?app_ecm_workflowview_detailsProcessInstanceID=" + numSolicitacao;
+
+    return urlSolicitacao;
+}
+
+function getServerURL() {
+    var ds = DatasetFactory.getDataset("dsGetServerURL", null, null, null);
+    return ds.getValue(0, "URL");
 }
