49e044fbd590ac40d6e941f73d613022360deb36
ClerivanBF <clerivan.falcao@castilho.com.br>
2026-06-15 08:44:20 -0300

    Script.js : Adicionado permissões para todas as Obras para clerivan.falcao e roney.tomm; Alterado as chamadas do preenchimento de modelos para quando tiver vazio; Adicionado a class form-control quando VIEW, no campo Classe Operacional; Limpa campos básicos de dados dos equips quando feito change de modelo; Adicionado biding do selectize no campo de Classe Operacional; Feito condição que ao alterar qualquer campo que seja Horas ou KM, sem alterem para ficar um só; E adicionados a condição para não aplicar quando PA (porque coisas que o PA não ultiliza); E não pede anexo quando PA

--- Cadastro-de-Equipamentos-V2/forms/formCadastroDeEquipamentos/js/script.js
+++ Cadastro-de-Equipamentos-V2/forms/formCadastroDeEquipamentos/js/script.js
@@ -48,12 +48,17 @@ $(document).ready(async function () {
 async function loadTelaInicio(){
     if ($("#userCode").val() == "FlavioHerculano") {
         preenchePermissoesDoUsuario(true);
-    }else{
+
+    } else if ($("#userCode").val() == "roney.tomm" || $("#userCode").val() == "clerivan.falcao") { 
+        preenchePermissoesDoUsuario(true);
+
+    } else {
         preenchePermissoesDoUsuario();
     }
+
     insereOptionsDosFornecedores();
     $(".inputPA, .inputOutros, .inputMA").closest("div.inputGroup").hide();
-    modelos = await promiseBuscaModelosDeEquipamentosDoSisma();
+    modelos = $("#categoria").val() ? await promiseBuscaModelosDeEquipamentosDoSisma($("#categoria").val()) : [];
     preencheOptionsDosModelos();
     $("#historico").hide();
     FLUIGC.calendar('#dataChegadaObra');
@@ -66,13 +71,12 @@ async function loadTelaInicio(){
     $("#consumoMedio").maskMoney({ thousands: '', decimal: '.' });
 
 }
-
 async function loadTelaAjuste() {
     preenchePermissoesDoUsuario();
     insereOptionsDosFornecedores();
     asyncMontaHistorico();
     $("#tipoAnexo").val("");
-    modelos = await promiseBuscaModelosDeEquipamentosDoSisma();
+    modelos = $("#categoria").val() ? await promiseBuscaModelosDeEquipamentosDoSisma($("#categoria").val()) : [];
     preencheOptionsDosModelos();
     FLUIGC.calendar('#dataChegadaObra');
     FLUIGC.calendar('#dataVencimentoAnexo');
@@ -96,9 +100,8 @@ async function loadTelaAjuste() {
         $("#divValorMaoDeObra").show();
     }
 }
-
 async function loadTelaCentralDeEquipamentos() {
-    modelos = await promiseBuscaModelosDeEquipamentosDoSisma();
+    modelos = await promiseBuscaModelosDeEquipamentosDoSisma($("#categoria").val());
     preencheOptionsDosModelos();
     insereOptionsDosFornecedores();
     preenchePermissoesDoUsuario(true);
@@ -127,12 +130,11 @@ async function loadTelaCentralDeEquipamentos() {
         $("#divValorMaoDeObra").show();
     }
 }
-
 async function loadTelaQSST() {
     asyncMontaHistorico();
     geraAnexos();
     geraTabelaCaracteristicasTecnicas();
-    modelos = await promiseBuscaModelosDeEquipamentosDoSisma();
+    modelos = await promiseBuscaModelosDeEquipamentosDoSisma($("#categoria").val());
     preencheOptionsDosModelos();
     alteraCategoriaDaSolicitacao($("#categoria").val());
     $("#divOpcoesAprovacao").show();
@@ -145,14 +147,13 @@ async function loadTelaQSST() {
         $("#divValorMaoDeObra").show();
     }
 }
-
 async function loadTelaVIEW() {
     asyncMontaHistorico();
     geraAnexos();
     geraTabelaCaracteristicasTecnicas();
     alteraCategoriaDaSolicitacao($("#categoria").text());
 
-    $("#coligada, #obra, #modelo, #fornecedor").addClass("form-control");
+    $("#coligada, #obra, #modelo, #classOperacional, #fornecedor").addClass("form-control");
 
     if ($("#checkboxTemMaoDeObra").is(":checked")) {
         $("#divValorMaoDeObra").show();
@@ -204,13 +205,18 @@ function bindings() {
         }
     });
 
-    $("#categoria").on("change", function () {
+    $("#categoria").on("change", async function () {
         var categoria = $(this).val();
-        alteraCategoriaDaSolicitacao(categoria);
-    });
 
-    $("#modelo").on("change", function () {
-        preencheInformacoesDoModelo($(this).val());
+        // Limpa o campo a cada troca (change) de categoria
+        $("#modelo").val("");
+        $("#fabricante").val("");
+        $("#classeMecanica").val("");
+        $("#classOperacional").val("");
+
+        alteraCategoriaDaSolicitacao(categoria);
+        modelos = await promiseBuscaModelosDeEquipamentosDoSisma(categoria);
+        preencheOptionsDosModelos();
     });
 
     $("#coligada").selectize({
@@ -281,6 +287,20 @@ function bindings() {
         }
     });
 
+    $("#classOperacional").selectize({
+        onChange: function(value) {
+            if (!value) {
+                $("#IDCLOP").val("");
+                return;
+            }
+
+            const [ID_CLASSEOPERACIONAL] = value.split(" - ");
+            $("#IDCLOP").val(ID_CLASSEOPERACIONAL);
+
+            preencheModelosPorClasseOperacional(ID_CLASSEOPERACIONAL);
+        }
+    });
+
     $("#tipoAnexo").on("change", function(){
         if ($(this).val() == "Laudo Técnico" || $(this).val() == "ART") {
             $("#divDataVencimentoAnexo").show();
@@ -326,6 +346,31 @@ function bindings() {
             $(this).val("");
         }
 
+    });
+
+    $("#tipoKmChegadaObra").on("change", function () {
+        var val = $(this).val();
+
+        if (val == "KM") {
+            $("#tipoConsumoMedio").val("km/L");
+
+        // Caso contrário, será Horas
+        } else  {
+            $("#tipoConsumoMedio").val("L/H");
+        }
+    });
+
+    // Preenche campo de Medidor (KM / Hora) do Diesel S-500
+    $("#tipoConsumoMedio").on("change", function () {
+        var val = $(this).val();
+
+        // Preenche o campo de medidor do KM/Horas do Chegada na Obra, para todos serem um tipo de medidor só.
+        if (val == "km/L") {
+            $("#tipoKmChegadaObra").val("KM");
+
+        } else {
+            $("#tipoKmChegadaObra").val("Horas");
+        }
     });
 
     $("#AnoFabricacao").mask("9999");
@@ -404,7 +449,7 @@ var beforeSendValidate = function () {
         if (!$("#descricaoEquipamento").val()) {
             errorMessage.push("Informe a Descrição do Equipamento");
         }
-        if (!$("#prefixo").val()) {
+        if (!$("#prefixo").val() && $("#categoria") != "Outros") { // Não obriga/pede prefixo quando categoria "Outros"
             errorMessage.push("Informe o Prefixo");
         }
 
@@ -426,10 +471,10 @@ var beforeSendValidate = function () {
             if (!$("#placa").val() && !$("#chassi").val()) {
                 errorMessage.push("Informe a Placa ou o Chassi");
             }
-            if (!$("#potenciaMotor").val()) {
+            if (!$("#potenciaMotor").val() && $("#categoria").val() != "PA") {
                 errorMessage.push("Informe a Potência do Motor");
             }
-            if (!$("#tipoPotenciaMotor").val()) {
+            if (!$("#tipoPotenciaMotor").val() && $("#categoria").val() != "PA") {
                 errorMessage.push("Informe o Tipo da Potência do Motor");
             }
         }
@@ -467,10 +512,14 @@ var beforeSendValidate = function () {
             if (!$("#tipoCombustivel").val()) {
                 errorMessage.push("Informe o Tipo de combustível");
             }
-            if (!$("#litrosTanque").val()) {
+
+            // A Central de Equip não controla capacidade tanque quando PA
+            if (!$("#litrosTanque").val() && $("#categoria").val() != "PA") {
                 errorMessage.push("Informe a Capacidade do Tanque");
             }
-            if (!$("#consumoMedio").val()) {
+
+            // A Central de Equip não controla consumo quando PA
+            if (!$("#consumoMedio").val() && $("#categoria").val() != "PA") {
                 errorMessage.push("Informe o Consumo Médio");
             }
         }
@@ -499,13 +548,15 @@ var beforeSendValidate = function () {
         // Ou estiver na atividade QSST e a decisão deles for "Aprovado"
         // Espeficficamente "Aprovado" pra não impedir de reprovar e voltar ao solicitante por falta de anexo.
         if (atividade == ATIVIDADES.INICIO || atividade == ATIVIDADES.INICIO_0 || (atividade == ATIVIDADES.QSST && $("#decisao").val() == "Aprovado")) {
-            if ($("#anexosDocumentosEquipamento").val() == "") {
-                errorMessage.push("Anexe a Documentação do Equipamento");
-            }
-            if ($("#anexosFotosEquipamentos").val() == "") {
-                errorMessage.push("Anexe a Foto do Equipamento");
-            }
-            if ($("#categoria").val() != "Outros") {
+
+            // A Central de Equip não pede anexo quando PA
+            if ($("#categoria").val() != "Outros" && $("#categoria").val() != "PA") {
+                if ($("#anexosDocumentosEquipamento").val() == "") {
+                    errorMessage.push("Anexe a Documentação do Equipamento");
+                }
+                if ($("#anexosFotosEquipamentos").val() == "") {
+                    errorMessage.push("Anexe a Foto do Equipamento");
+                }
                 if ($("#anexosLaudoTecnico").val() == "") {
                     errorMessage.push("Anexe o Laudo Técnico");
                 }
